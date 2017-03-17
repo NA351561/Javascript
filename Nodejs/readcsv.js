24e5c2d23e4894578a7ab0980edc2d5f5044d446
na351561 <nandhinikaruppannan@gmail.com>
2017-03-17 17:33:11 +0530

    line and stacked chart

--- Nodejs/readcsv.js
+++ Nodejs/readcsv.js
@@ -38,8 +38,8 @@ function gettingData(readcsv)
 					{
 						temp=0;
 					}
-					oilobj[readdata[0]]=readdata[i];
-					oilobj['3-2013']=Number(temp); //store key value pair
+					oilobj[readdata[0]]=readdata[i].replace('Agricultural Production Oilseeds ','');
+					oilobj['value']=Number(temp); //store key value pair
 					oilobjarr.push(oilobj);  // pushing object values into array
 
 				}
@@ -71,8 +71,8 @@ function gettingData(readcsv)
 						{
 							temp=0;
 						}
-					foodobj[readdata[0]]=readdata[i];
-					foodobj['3-2013']=Number(temp); //storing read values as key value pair
+					foodobj[readdata[0]]=readdata[i].replace('Agricultural Production Foodgrains ','');
+					foodobj['value']=Number(temp); //storing read values as key value pair
 					foodobjarr.push(foodobj); //pushing object into array
 				}
 
@@ -115,7 +115,7 @@ function gettingData(readcsv)
         comobjarr.push(comobj);  //push all the values into array
 			}
 
-			fs.writeFileSync('CommercialCropsVsProduction.json',JSON.stringify(comobjarr)); //converting into JSON
+			fs.writeFileSync('CommercialCrops.json',JSON.stringify(comobjarr)); //converting into JSON
 			console.log('Aggregate value of Commercial crops for all the years is converted into JSON\n');
 		}
 
@@ -124,33 +124,30 @@ function gettingData(readcsv)
 		function southStates()
 		{
       var statearr=[]; //store all state values
-			for(var i=25;i<readdata.length;i=i+26) //iterating to get all the values
-			{
-
+      for(fieldvalue=3;fieldvalue<25;fieldvalue++)
+      {
           var stateobj={}; //empty object to store values
-					//read only four states values
-					var s1=(readdata[i].match(/Agricultural Production Foodgrains Rice Yield Andhra Pradesh/i)||[]).length;
-					var s2=(readdata[i].match(/Agricultural Production Foodgrains Rice Yield Karnataka/i)||[]).length;
-					var s3=(readdata[i].match(/Agricultural Production Foodgrains Rice Yield Kerala/i)||[]).length;
-					var s4=(readdata[i].match(/Agricultural Production Foodgrains Rice Yield Tamil Nadu/i)||[]).length;
-					if(s1>0||s2>0||s3>0||s4>0)
-					{
-            stateobj['Particulars']=readdata[i].replace('Agricultural Production Foodgrains Rice Yield ','');
-
-							var temp=readdata[f+i+1];
-							if(temp=='NA')
-							{
-								temp=0;
-							}
-
-							stateobj[readdata[f]]=Number(temp);  //store values for all year
-
-						}
-            statearr.push(stateobj); //push into array
-					}
-
-			}
-			fs.writeFileSync('SouthernStates.json',JSON.stringify(statearr)); //convert into JSON
-			console.log('Rice Production of 4 Southern States is converted into JSON');
+          for(var i=25;i<readdata.length;i=i+26) //iterating to get all the values
+          {
+            stateobj['year']=readdata[fieldvalue];
+            //read only four states values
+            var s1=(readdata[i].match(/Agricultural Production Foodgrains Rice Yield Andhra Pradesh/i)||[]).length;
+            var s2=(readdata[i].match(/Agricultural Production Foodgrains Rice Yield Karnataka/i)||[]).length;
+            var s3=(readdata[i].match(/Agricultural Production Foodgrains Rice Yield Kerala/i)||[]).length;
+            var s4=(readdata[i].match(/Agricultural Production Foodgrains Rice Yield Tamil Nadu/i)||[]).length;
+            if(s1>0||s2>0||s3>0||s4>0)
+            {
+              var temp=readdata[fieldvalue+i+1];
+              if(temp=='NA')
+              {
+                temp=0;
+              }
+              stateobj[readdata[i].replace('Agricultural Production Foodgrains Rice Yield ','')]=Number(temp);
+            }
+          }
+          statearr.push(stateobj); //push into array
+      }
+      fs.writeFileSync('SouthernStates.json',JSON.stringify(statearr)); //convert into JSON
+      console.log('Rice Production of 4 Southern States is converted into JSON');
 		}
 }
